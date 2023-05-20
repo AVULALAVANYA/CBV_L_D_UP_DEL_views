2874c19b68714811752ef294191f009103d5a20b
Avula Lavanya <arlavanya123@gmail.com>
2023-05-20 18:11:26 +0530

    first commit

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class School(models.Model):
     name=models.CharField(max_length=100)
@@ -8,6 +8,9 @@ class School(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('School',kwargs={'pk':self.pk})
 
 class students(models.Model):
     school=models.ForeignKey(School, on_delete=models.CASCADE,related_name='students')
